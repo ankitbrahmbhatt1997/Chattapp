cdf35f04bf9e11a7016d4961fa2885427144057e
Ankit Brahmbhatt <ankitbrahmbhatt1997@gmail.com>
2018-05-11 16:19:28 +0530

    Almost Done

--- public/Js/chat.js
+++ public/Js/chat.js
@@ -56,7 +56,8 @@ socket.on("updatedUsersList", function(users) {
   usersCol.innerHTML = `<p class="flow-text white-text">Active Users</p>`;
   users.forEach(user => {
     let oneUser = document.createElement("div");
-    oneUser.classList.add("card-panel");
+    oneUser.classList.add("white");
+    oneUser.classList.add("custom-user");
     oneUser.classList.add("blue-text");
     oneUser.innerHTML = `${user}`;
     usersCol.appendChild(oneUser);
